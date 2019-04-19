884b79a24ae7add791f9b586525e53b8fe3edebb
Adriel <49051133+izuca@users.noreply.github.com>
2019-04-19 12:35:24 -0300

    Create ex3.c

---
+++ lista1B/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+	int numero;
+	
+	scanf("%i",&numero);
+	
+	if (numero % 3 == 0 && numero % 3 == 0)
+		printf("O NUMERO E DIVISIVEL\n");
+	else
+		printf("O NUMERO NAO E DIVISIVEL\n");
+	return 0;
+}
