58a405b1a6faca66cb26d9191690f933e5ebbf59
Adriel <49051133+izuca@users.noreply.github.com>
2019-06-11 16:39:30 -0300

    Create ex11(incompleto).c

---
+++ Matrizes/ex11(incompleto).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int l,c,camisa,extremidade,pe,gorro;
+    camisa = 1111;
+    extremidade = 0;
+    pe = 8;
+    gorro = 4;
+
+    scanf("%i %i",&l,&c);
+
+    int matriz[l][c];
+
+    for(int linha = 0; linha < l;linha++)
+    {
+        for(int coluna = 0; coluna < c;coluna++)
+            scanf("%i %i",&matriz[linha][coluna]);
+    }
+
+    for(int linha = 0; linha < l;linha++)
+    {
+        for(int coluna = 0; coluna < c;coluna++)
+        {
+            if()
+        }
+    }
+
+    return 0;
+}
